93374735c245e90042b007a32c1fc4f2a91cf2d8
rebfarish <rebfarish@gmail.com>
2018-10-11 22:21:08 -0600

    Added landscape view, changed button formatting, and saved data across rotation.

--- app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,6 +13,9 @@ import android.widget.Toast;
 
 
 public class QuizActivity extends AppCompatActivity {
+
+  private static final String TAG = "QuizActivity";
+  private static final String KEY_INDEX = "index";
 
   private Button mTrueButton;
   private Button mFalseButton;
@@ -37,10 +41,15 @@ private int mCurrentIndex = 0;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate(Bundle) called");
     setContentView(R.layout.activity_quiz);
     correctTally = findViewById(R.id.correct_tally);
     incorrectTally = findViewById(R.id.incorrect_tally);
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+    if (savedInstanceState !=null){
+      mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+    }
+
 
 
     mTrueButton = (Button) findViewById(R.id.true_button);
@@ -71,6 +80,44 @@ private int mCurrentIndex = 0;
     });
     updateQuestion();
   }
+
+  @Override
+  public void onStart(){
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
+
+  @Override
+  public void onResume(){
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
+
+  @Override
+  public void onPause(){
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+    Log.i(TAG, "onSaveInstanceState");
+    savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+  }
+
+  @Override
+  public void onStop(){
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  public void onDestroy(){
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
+  }
+
   private void updateQuestion(){
     int question = mQuestionBank[mCurrentIndex].getTextResId();
     mQuestionTextView.setText(question);
@@ -94,6 +141,7 @@ private int mCurrentIndex = 0;
     }
     Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
         .show();
+
 
 
   }
