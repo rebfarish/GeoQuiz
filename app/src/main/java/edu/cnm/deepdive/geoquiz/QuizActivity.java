9ba7235c65fb8c86d0f054642bf885ced2017063
rebfarish <rebfarish@gmail.com>
2018-10-28 18:02:08 -0600

    Added cheater sub-activity, cheat button, and a toast when a user is caught cheating.

--- app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
@@ -1,5 +1,8 @@
 package edu.cnm.deepdive.geoquiz;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,11 +14,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-
 public class QuizActivity extends AppCompatActivity {
 
   private static final String TAG = "QuizActivity";
   private static final String KEY_INDEX = "index";
+  private static final int REQUEST_CODE_CHEAT = 0;
 
   private Button mTrueButton;
   private Button mFalseButton;
@@ -25,18 +28,20 @@ public class QuizActivity extends AppCompatActivity {
   private TextView incorrectTally;
   private int correct;
   private int incorrect;
+  private Button mCheatButton;
 
-private Question [] mQuestionBank = new Question[]{
-    new Question(R.string.question_australia, true),
-    new Question(R.string.question_oceans, true),
-    new Question(R.string.question_mideast, false),
-    new Question(R.string.question_africa, false),
-    new Question(R.string.question_americas, true),
-    new Question(R.string.question_asia, true),
+  private Question[] mQuestionBank = new Question[]{
+      new Question(R.string.question_australia, true),
+      new Question(R.string.question_oceans, true),
+      new Question(R.string.question_mideast, false),
+      new Question(R.string.question_africa, false),
+      new Question(R.string.question_americas, true),
+      new Question(R.string.question_asia, true),
 
-};
+  };
 
-private int mCurrentIndex = 0;
+  private int mCurrentIndex = 0;
+  private boolean mIsCheater;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +51,10 @@ private int mCurrentIndex = 0;
     correctTally = findViewById(R.id.correct_tally);
     incorrectTally = findViewById(R.id.incorrect_tally);
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-    if (savedInstanceState !=null){
+
+    if (savedInstanceState != null) {
       mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
     }
-
-
 
     mTrueButton = (Button) findViewById(R.id.true_button);
     mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -70,31 +74,56 @@ private int mCurrentIndex = 0;
     });
 
     mNextButton = (Button) findViewById(R.id.next_button);
-    mNextButton.setOnClickListener(new View.OnClickListener(){
+    mNextButton.setOnClickListener(new View.OnClickListener() {
       @Override
-      public void onClick(View v){
+      public void onClick(View v) {
         mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        mIsCheater = false;
         updateQuestion();
-
       }
     });
+
+    mCheatButton = (Button) findViewById(R.id.cheat_button);
+    mCheatButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        //Start CheatActivity
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+        startActivityForResult(intent, REQUEST_CODE_CHEAT);
+      }
+    });
+
     updateQuestion();
   }
 
   @Override
-  public void onStart(){
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (resultCode != Activity.RESULT_OK) {
+      return;
+    }
+    if (requestCode == REQUEST_CODE_CHEAT) {
+      if (data == null) {
+        return;
+      }
+      mIsCheater = CheatActivity.wasAnswerShown(data);
+    }
+  }
+
+  @Override
+  public void onStart() {
     super.onStart();
     Log.d(TAG, "onStart() called");
   }
 
   @Override
-  public void onResume(){
+  public void onResume() {
     super.onResume();
     Log.d(TAG, "onResume() called");
   }
 
   @Override
-  public void onPause(){
+  public void onPause() {
     super.onPause();
     Log.d(TAG, "onPause() called");
   }
@@ -107,18 +136,18 @@ private int mCurrentIndex = 0;
   }
 
   @Override
-  public void onStop(){
+  public void onStop() {
     super.onStop();
     Log.d(TAG, "onStop() called");
   }
 
   @Override
-  public void onDestroy(){
+  public void onDestroy() {
     super.onDestroy();
     Log.d(TAG, "onDestroy() called");
   }
 
-  private void updateQuestion(){
+  private void updateQuestion() {
     int question = mQuestionBank[mCurrentIndex].getTextResId();
     mQuestionTextView.setText(question);
     correctTally.setText(getString(R.string.correct_tally_format, correct));
@@ -130,18 +159,22 @@ private int mCurrentIndex = 0;
 
     int messageResId = 0;
 
-    if (userPressedTrue == answerIsTrue) {
-      messageResId = R.string.correct_toast;
-      correct++;
+    if (mIsCheater) {
+      messageResId = R.string.judgement_toast;
+    } else {
 
-    }else {
-      messageResId = R.string.incorrect_toast;
-      incorrect++;
+      if (userPressedTrue == answerIsTrue) {
+        messageResId = R.string.correct_toast;
+        correct++;
 
+      } else {
+        messageResId = R.string.incorrect_toast;
+        incorrect++;
+
+      }
     }
     Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
         .show();
-
 
 
   }
